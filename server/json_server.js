ff691b763e6de8b13b4bffb75ea10dac839319af
Viraj Ranjankumar <viraj.ranjankumar@gmail.com>
2019-11-02 01:47:34 +0400

    new web server -  not used

--- server/json_server.js
+++ server/json_server.js
@@ -1,19 +1,21 @@
 const jsonServer = require('json-server')
 const path = require('path')
 // const autocomplete  = require('../templates/api/autocomplete.json');
-const carrefour = require('../deals/carrefour.json');
-const choithram = require('../deals/choithram.json');
-const unioncoop = require('../deals/unioncoop.json');
-const lulu = require('../deals/lulu.json');
-const emax = require('../deals/emax.json');
-const favorite = require('../deals/favorite.json');
+const requireJSON = f => require(`../deals/${f}.json`).map(o => ({...o, source: f}))
+const carrefour = requireJSON('carrefour');
+const choithrams = requireJSON('choithrams');
+const unioncoop = requireJSON('unioncoop');
+const lulu = requireJSON('lulu');
+const emax = requireJSON('emax');
+const querystring = require('querystring');
+// const favorite = require('../deals/favorite.json');
 
 const [ port = 3000 ] = process.argv.slice(2);
 
 const autocomplete = unioncoop.map(({description, name}) => ({description, name, source: 'unioncoop'} ))
                   .concat(lulu.map(({description, name}) => ({description, name, source: 'lulu'})))
                   .concat(emax.map(({description, name}) => ({description, name, source: 'emax'})))
-                  .concat(choithram.map(({description, name}) => ({description, name, source: 'choithram'})))
+                  .concat(choithrams.map(({description, name}) => ({description, name, source: 'choithrams'})))
                   .concat(carrefour.map(({description, name}) => ({description, name, source: 'carrefour'})))
                   // .concat(Array.from(new Set(
                   //   carrefour.reduce((acc, {category}) => 
@@ -23,24 +25,24 @@ const server = jsonServer.create()
 const router = jsonServer.router(function() {
   return {
       autocomplete,
-      favorite,
       carrefour,
       unioncoop,
       lulu,
       emax,
-      choithram,
-      all: [].concat.apply([], [lulu, emax, carrefour, choithram, unioncoop])
+      choithrams,
+      all: [].concat.apply([], [lulu, emax, carrefour, choithrams, unioncoop])
   }
 }())
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-server.get('/echo', (req, res) => res.jsonp(req.query))
+server.post('/echo', (req, res) => res.jsonp(req.query))
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    console.log(req.body)
-    // req.body.createdAt = Date.now()
-  }
+  const query = req.query;
+  delete query['__amp_source_origin']
+  query['_page'] = parseInt(query['_page']) + 1
+  const fullUrl = req.protocol + '://' + req.get('host') + req.path
+  req.nextPath = `${fullUrl}?${querystring.stringify(query)}`
   next()
 })
 
@@ -50,5 +52,6 @@ server.listen(port, () => {
 })
 
 router.render = (req, res) => {
-  res.jsonp({ items: res.locals.data })
+  const next = res.locals.data.length == 0 ? null : req.nextPath
+  res.jsonp({ items: res.locals.data, next })
 }
--- server/web_server.js
+++ server/web_server.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const productApiManager = require('./ApiManager.js');
 const formidableMiddleware = require('express-formidable');
 const sessions = require("client-sessions");
 const serializer = require('serialize-to-js');
 const ampCors = require('amp-toolbox-cors');
 const AmpOptimizer = require('amp-toolbox-optimizer');
-const apiManager = new productApiManager();
 
 /* CONSTANTS */
 const ampOptimizer = AmpOptimizer.create();
@@ -41,8 +39,7 @@ app.get('/shopping-cart', function (req, res) {
     let cartItems = shoppingCart.cartItems;
     if (cartItems.length > 0) {
       let firstCartItem = cartItems[0];
-      relatedProductsObj.Main_Id = firstCartItem.productId;
-      relatedProductsObj.CategoryId = firstCartItem.categoryId;
+      relatedProductsObj.id = firstCartItem.productId;
     }
   }
 
@@ -55,17 +52,8 @@ app.get('/shopping-cart', function (req, res) {
 
 app.post('/api/add-to-cart', function (req, res) {
 
-  let productId = req.fields.productId;
-  let categoryId = req.fields.categoryId;
-  let name = req.fields.name;
-  let price = req.fields.price;
-  let color = req.fields.color;
-  let size = req.fields.size;
-  let imgUrl = req.fields.imgUrl;
-  let origin = req.get('origin');
-  let quantity = req.fields.quantity;
-  let cannonicalUrl = 'https://' + req.get('host');
   //If comes from the cache
+  /*
   if (req.headers['amp-same-origin'] !== 'true') {
     //transfrom POST into GET and redirect to same url
     let queryString = 'productId=' + productId + '&categoryId=' + categoryId + '&name=' + name + '&price=' + price + '&color=' + color + '&size=' + size + '&quantity=' + quantity + '&origin=' + origin + '&imgUrl=' + imgUrl;
@@ -74,34 +62,21 @@ app.post('/api/add-to-cart', function (req, res) {
     updateShoppingCartOnSession(req, productId, categoryId, name, price, color, size, imgUrl, quantity);
     res.header("AMP-Redirect-To", origin + "/shopping-cart");
   }
-
+  */
+  updateShoppingCartOnSession(req, req.fields.productId, req.fields.source);
   //amp-form requires json response
   res.json({});
 });
 
 app.get('/api/add-to-cart', function (req, res) {
-  const {
-    productId,
-    categoryId,
-    name,
-    price,
-    color,
-    size,
-    imgUrl,
-    quantity
-  } = req.query;
-
-  updateShoppingCartOnSession(req, productId, categoryId, name, price, color, size, imgUrl, quantity);
+  updateShoppingCartOnSession(req, req.query.productId, req.query.source);
   res.redirect('/shopping-cart');
 });
 
 // Retrieve the shopping cart from session, and wrap it into an 'items' array, which is the format expected by amp-list.
 app.get('/api/cart-items', function (req, res) {
-  let cart = getCartFromSession(req);
-
-  let response = { items: [cart] };
-
-  res.send(response);
+  const cart = getCartFromSession(req);
+  res.send({ items: [cart] });
 });
 
 app.get('/api/cart-count', function (req, res) {
@@ -110,13 +85,13 @@ app.get('/api/cart-count', function (req, res) {
 });
 
 app.post('/api/delete-cart-item', function (req, res) {
-  const { productId, color, size } = req.fields;
+  const { productId } = req.fields;
 
   let shoppingCartResponse = { items: [] };
 
   if (shoppingCart) {
     shoppingCart = serializer.deserialize(shoppingCart);
-    shoppingCart.removeItem(productId, color, size);
+    shoppingCart.removeItem(productId);
     session.shoppingCart = serializer.serialize(shoppingCart);
     shoppingCartResponse.items.push(shoppingCart);
   }
@@ -132,18 +107,43 @@ app.post('/api/delete-cart-item', function (req, res) {
 function getCartFromSession({ session: { shoppingCart } }) {
   return (shoppingCart)
   ? serializer.deserialize(shoppingCart)
-  : apiManager.createCart();
+  : createCart();
 }
 
 //Create a cart new item. If the cart exists in the session, add it there.
 //Otherwise, create a new cart and add the recently created item to the session.
-function updateShoppingCartOnSession(req, productId, categoryId, name, price, color, size, imgUrl, quantity) {
-  let cartProduct = apiManager.createCartItem(productId, categoryId, name, price, color, size, imgUrl, quantity);
+function updateShoppingCartOnSession(req, productId, source) {
+  let cartProduct = { productId, source };
   let shoppingCartJson = req.session.shoppingCart;
   let shoppingCartObj = (shoppingCartJson)
     ? serializer.deserialize(shoppingCartJson)
-    : apiManager.createCart();
-
+    : createCart();
+  
   shoppingCartObj.addItem(cartProduct);
   req.session.shoppingCart = serializer.serialize(shoppingCartObj);
+}
+
+function createCart() {
+  return {
+    cartItems: [],
+    isEmpty: true,
+    addItem: function (item) {
+      //check if item exists in cart before pushing
+      var foundItem = this.cartItems.filter(e => e.productId == item.productId);
+      if (foundItem.length === 0) {
+        this.cartItems.push(item);
+        this.isEmpty = false;
+      }
+    },
+    removeItem: function (productId) {
+      for (var i = 0; i < this.cartItems.length; i++) {
+        if (this.cartItems[i].productId === productId) {
+          this.cartItems.splice(i, 1);
+          if (this.cartItems.length == 0) {
+            this.isEmpty = true;
+          }
+        }
+      }
+    }
+  };
 }
